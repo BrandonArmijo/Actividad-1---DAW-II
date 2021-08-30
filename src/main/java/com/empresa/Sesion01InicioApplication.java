87c665c5f57ebea96a6e8072450aa8da16355cb9
BrandonArmijo <brandon.armijo96@gmail.com>
2021-08-30 11:35:29 -0500

    Primer commit

--- src/main/java/com/empresa/Sesion01InicioApplication.java
+++ src/main/java/com/empresa/Sesion01InicioApplication.java
@@ -2,8 +2,10 @@ package com.empresa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.empresa.controller","MedicamentoRestController","MedicamentoService"})
 public class Sesion01InicioApplication {
 
 	public static void main(String[] args) {
